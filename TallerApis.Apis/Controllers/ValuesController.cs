656e5ce9b4c478ffffaef1d86d3781cdb1e2ac1f
Kevin Andres Gomez Hernandez <kevinandres1513@outlook.es>
2018-10-10 19:25:24 -0500

    527

--- TallerApis.Apis/Controllers/ValuesController.cs
+++ TallerApis.Apis/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace TallerApis.Apis.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value 2" };
+            return new string[] { "value1", "value k2" };
         }
 
         // GET api/values/5
